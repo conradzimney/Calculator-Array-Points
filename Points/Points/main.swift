9e8f1c4c92ad8cced69b78caa471c24512c0c30b
Conrad Z <conradzimney@gmail.com>
2015-10-11 15:52:14 -0700

    started points, still needs work

--- Points/Points/main.swift
+++ Points/Points/main.swift
@@ -8,5 +8,35 @@
 
 import Foundation
 
-print("Hello, World!")
+let point1 = (1,2)
+let point2 = (3,4)
+let point3 = (2.5, 5.7)
+let point4 = (3.2, 4.5)
 
+let addPoints = {
+    (point1 : (x : Int ,y : Int), point2 : (x : Int,y : Int)) -> (Int, Int) in
+    // implement two-non-arity point handling here
+    
+    
+    return (point1.x + point2.x, point1.y + point2.y)
+}
+
+let subtractPoints = {
+    (point1 : (x : Int ,y : Int), point2 : (x : Int,y : Int)) -> (Int, Int) in
+    // implement two-non-arity point handling here
+    
+    
+    return (point1.x - point2.x, point1.y - point2.y)
+}
+
+
+let point1Dict = ["x": "y"]
+
+
+let addDictPoints = {
+    
+}
+
+let subractDictPoints = {
+    
+}
